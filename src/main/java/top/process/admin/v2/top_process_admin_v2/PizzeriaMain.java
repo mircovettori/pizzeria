e903de0860634fe8a3b5c65ff6f3fd393e4b7fdf
Mirco VETTORI <mirco.vettori@alten.it>
2024-09-30 11:43:53 +0200

    first commit

---
+++ src/main/java/top/process/admin/v2/top_process_admin_v2/PizzeriaMain.java
@@ -0,0 +1,15 @@
+package top.process.admin.v2.top_process_admin_v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan
+@SpringBootApplication
+public class PizzeriaMain {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzeriaMain.class, args);
+	}
+
+}
---
+++ src/test/java/top/process/admin/v2/top_process_admin_v2/PizzeriaMainTests.java
@@ -0,0 +1,13 @@
+package top.process.admin.v2.top_process_admin_v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzeriaMainTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
